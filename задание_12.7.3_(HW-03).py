14b4a3c6c25aabea31817531219e2c6fd1aa6682
Tagir <123125929+pythonautomationengineer@users.noreply.github.com>
2023-02-17 01:20:25 +0300

    Add files via upload

---
+++ задание_12.7.3_(HW-03).py
@@ -0,0 +1,17 @@
+def main():
+    sum_sum = int(input("Введите сумму, которую планируете положить под проценты: "))
+    per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+    percent_list = list(per_cent.values())  # список значений словаря
+    deposit = [int(percent_list[0] * sum_sum / 100), int(percent_list[1] * sum_sum / 100),
+               int(percent_list[2] * sum_sum / 100), int(percent_list[3] * sum_sum / 100)]
+    print(f'Размеры депозитов при заданной сумме: {deposit}')
+    max_deposit(deposit)  # передадим депозиты в банках в функцию max_deposit
+
+
+def max_deposit(deposit):
+    deposit_max = deposit.index(max(deposit))  # получим индекс максимального значения депозита
+    print(f'Максимальная сумма, которую вы можете заработать — {deposit[deposit_max]}')
+
+
+if __name__ == '__main__':
+    main()
