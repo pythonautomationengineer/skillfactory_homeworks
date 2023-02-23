b597cc6ba5d00020995f9dd64fdcc48feb9d0622
Tagir <123125929+pythonautomationengineer@users.noreply.github.com>
2023-02-24 02:20:17 +0300

    Update задание_12.7.3_(HW-03).py

--- задание_12.7.3_(HW-03).py
+++ задание_12.7.3_(HW-03).py
@@ -1,7 +1,7 @@
 def main():
     sum_sum = int(input("Введите сумму, которую планируете положить под проценты: "))
     per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-    percent_list = list(per_cent.values())  # список значений словаря
+    percent_list = list(per_cent.values()) 
     deposit = [int(percent_list[0] * sum_sum / 100), int(percent_list[1] * sum_sum / 100),
                int(percent_list[2] * sum_sum / 100), int(percent_list[3] * sum_sum / 100)]
     print(f'Размеры депозитов при заданной сумме: {deposit}')
